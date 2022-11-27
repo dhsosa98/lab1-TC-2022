ffc25e335c77029b976b2a9fce91abeb1bb71cea
Diego <dhsosa98@gmail.com>
2022-11-27 18:51:13 -0300

    add json format

--- lab1_tc2022.ino
+++ lab1_tc2022.ino
@@ -2,6 +2,10 @@
 
 #include <Stepper.h>
 
+#include <Wire.h>
+
+#include <ArduinoJson.h>
+
 #define SENSOR_TEMP_PIN 2
 
 #define DHTTYPE DHT11
@@ -25,13 +29,65 @@ float maxAngle = 180;
 String input;
 String selectedRibbon = "ribbon 1";
 
+String json;
+DynamicJsonDocument doc(1024);
+
+const byte I2C_SLAVE_ADDR = 0x20;
+
 void setup() {
     Serial.begin(9600);
     myStepper.setSpeed(motSpeed);
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
+    Wire.begin(I2C_SLAVE_ADDR);
+    Wire.onRequest(requestEvent);
+    Wire.onReceive(receiveEvent);
     dht.begin();
+}
+
+const char ASK_FOR_LENGTH = 'L';
+const char ASK_FOR_DATA = 'D';
+
+char request = ' ';
+char requestIndex = 0;
+
+void SerializeObject(JsonArray sensors, JsonArray actuators)
+{
+    doc["slave"] = "sklaakdl";
+    doc["date"] = "sklakl";
+    doc["actuators"] = actuators;
+    doc["sensors"] = sensors;
+    serializeJson(doc, json);
+}
+
+void receiveEvent(int bytes)
+{
+   while (Wire.available())
+   {
+     request = (char)Wire.read();
+   }
+}
+void requestEvent()
+{
+   if(request == ASK_FOR_LENGTH)
+   {
+      Wire.write(json.length());
+      char requestIndex = 0;
+   }
+   if(request == ASK_FOR_DATA)
+   {
+      if(requestIndex < (json.length() / 32)) 
+      {
+         Wire.write(json.c_str() + requestIndex * 32, 32);
+         requestIndex ++;
+      }
+      else
+      {
+         Wire.write(json.c_str() + requestIndex * 32, (json.length() % 32));
+         requestIndex = 0;
+      }
+   }
 }
 
 void loop() {   
@@ -92,6 +148,19 @@ void runStepperByTemperature()
     Serial.println(currentInterval);
     Serial.println("Interval Angle Degrees: ");
     Serial.println(currentInterval*(maxAngle/intervals));
+    StaticJsonDocument<200> actuator1;
+    StaticJsonDocument<200> sensor1;
+    DynamicJsonDocument docActuators(1024);
+    JsonArray arrActuators = docActuators.to<JsonArray>();
+    DynamicJsonDocument docSensors(1024);
+    JsonArray arrSensors = docSensors.to<JsonArray>();
+    actuator1["type"]="stepper";
+    actuator1["current_interval"]=currentInterval;
+    sensor1["type"]="temperature";
+    sensor1["current_value"]=t;
+    arrActuators[0]=actuator1;
+    arrSensors[0]=sensor1;
+    SerializeObject(arrSensors, arrActuators);
 }
 
 void pass_right(){
@@ -111,28 +180,58 @@ void measureByHumidity(){
      Serial.println("Failed to read from DHT sensor!");
      return;
   }
+  StaticJsonDocument<200> actuator1;
+  StaticJsonDocument<200> actuator2;
+  StaticJsonDocument<200> actuator3;
+  StaticJsonDocument<200> sensor1;
+  DynamicJsonDocument docActuators(1024);
+  JsonArray arrActuators = docActuators.to<JsonArray>();
+  DynamicJsonDocument docSensors(1024);
+  JsonArray arrSensors = docSensors.to<JsonArray>();
+  actuator1["type"]="led";
+  actuator2["type"]="led";
+  actuator3["type"]="led";
+  sensor1["type"]= "humidity";
+  sensor1["current_value"]= h;
   if (h<=25){
     // turn off all leds
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
+      actuator1["current_state"]="off";
+      actuator2["current_state"]="off";
+      actuator3["current_state"]="off";
     }
     else if ((h>25) and (h<=50)){
       // turn on one led
       digitalWrite(3, HIGH);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
+      actuator1["current_state"]="on";
+      actuator2["current_state"]="off";
+      actuator3["current_state"]="off";
     }
     else if ((h>50) and (h<=75)) {
       // turn on two leds
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, LOW);
+      actuator1["current_state"]="on";
+      actuator2["current_state"]="on";
+      actuator3["current_state"]="off";
     }
     else {
       // turn on all leds
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
+      actuator1["current_state"]="on";
+      actuator2["current_state"]="on";
+      actuator3["current_state"]="on";
     }
+    arrActuators[0] = actuator1;
+    arrActuators[1] = actuator2;
+    arrActuators[2] = actuator3; 
+    arrSensors[0] = sensor1;
+    SerializeObject(arrSensors, arrActuators);
 }
